4ec9c2864b67ed57b0ab7a28d8e25c3032bf231c
Heron <heron@HerondeMacBook-Pro.local>
2017-06-19 20:24:40 +0800

    finish chapter 5

--- books/models.py
+++ books/models.py
@@ -15,7 +15,7 @@ class Publisher(models.Model):
 class Author(models.Model):
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=40)
-	email = models.EmailField()
+	email = models.EmailField(blank=True, verbose_name='e-mail')
 
 	def __str__(self):
 		return u'%s %s' % (self.first_name, self.last_name)
@@ -24,7 +24,7 @@ class Book(models.Model):
 	title = models.CharField(max_length=30)
 	authors = models.ManyToManyField(Author)
 	publisher = models.ForeignKey(Publisher)
-	publication_date = models.DateField()
+	publication_date = models.DateField(blank=True, null=True)
 
 	def __str__(self):
 		return self.title
